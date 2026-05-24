b464b262d767dd3d05200f9e7000c2c35a39a77a
huichieh <huichieh1123@gmail.com>
2026-05-24 16:04:28 +0800

    update: can't login message after exam

--- backend/internal/service/auth_service.go
+++ backend/internal/service/auth_service.go
@@ -39,7 +39,8 @@ func (s *AuthService) Login(username, password string) (string, error) {
 	// 4. 🌟 考試時間邏輯
 	var examExpiresAt int64
 	now := time.Now()
-	examDuration := 3 * time.Hour
+	// examDuration := 1 * time.Minute
+	examDuration := 1 * time.Hour
 
 	// 只有一般考生 (candidate) 需要計算 3 小時限制，admin 不需要
 	if user.Role == "candidate" {
