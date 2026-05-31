95a49bc3303acc3558a82106a61008e656910142
PhantomDestrier <phantomdestrier@gmail.com>
2026-05-31 18:01:16 +0800

    fix: prettier format

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom', // 啟用瀏覽器模擬
-    setupFiles: './vitest.setup.ts', // 載入剛剛建好的 setup 檔
+    environment: "jsdom", // 啟用瀏覽器模擬
+    setupFiles: "./vitest.setup.ts", // 載入剛剛建好的 setup 檔
   },
   server: {
     host: true,
